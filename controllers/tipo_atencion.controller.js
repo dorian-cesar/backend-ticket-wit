a2a5f8ea53fcd98676e65f5df01d7d1b4617a802
Dorian-Cesar <dorian.celu@gmail.com>
2025-06-27 13:14:08 -0400

    add categoria api.get all

--- controllers/tipo_atencion.controller.js
+++ controllers/tipo_atencion.controller.js
@@ -2,10 +2,12 @@ const db = require("../models/db");
 
 exports.listarTipos = (req, res) => {
   db.query(`
-    SELECT ta.id, ta.nombre, ta.area_id, a.nombre AS area_nombre, u.nombre AS ejecutor_nombre, u.id AS ejecutor_id
+    SELECT ta.id, ta.nombre, ta.area_id, a.nombre AS area_nombre, u.nombre AS ejecutor_nombre, u.id AS ejecutor_id, c.nombre AS categoria
     FROM tipo_atencion ta
     JOIN areas a ON ta.area_id = a.id
     JOIN users u ON ta.ejecutor_id = u.id
+     JOIN categorias c ON ta.categoria_id = c.id
+
   `, (err, results) => {
     if (err) return res.status(500).json({ error: "Error al listar tipos" });
     res.json(results);
